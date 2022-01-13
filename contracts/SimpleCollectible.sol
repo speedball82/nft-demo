3e7f18278a0dd42ce6cc6ef0769f3140c4cb1c65
Alessandro <user.name>
2022-01-13 17:20:44 +0100

    3 commit

---
+++ contracts/SimpleCollectible.sol
@@ -0,0 +1,15 @@
+// contracts/MyNFT.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract SimpleCollettible is ERC721 {
+
+
+    constructor() ERC721("Dogie", "DOG") public {
+    }
+
+
+
+}
